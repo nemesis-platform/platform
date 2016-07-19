80c0dd69f72acb8c87ccaee9dc0e86029dd66228
Pavel Batanov <batanov@banki.ru>
2016-07-19 17:47:38 +0300

    explicitly set dirs for sensiolabs fake kernel

--- .sensiolabs/AppKernel.php
+++ .sensiolabs/AppKernel.php
@@ -5,4 +5,18 @@
  */
 class AppKernel extends \NemesisPlatform\AppKernel
 {
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
+    public function getRootDir()
+    {
+        return dirname(__DIR__).'/app';
+    }
 }
