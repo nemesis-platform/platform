28b39fd6b91fd4930883355e5358baa8f4c01e20
Pavel Batanov <pavel@batanov.me>
2016-07-19 21:26:41 +0300

    remove flatten exception deprecations and reinvent the twig bundle exception shower

--- src/NemesisPlatform/Core/Account/Controller/ExceptionController.php
+++ src/NemesisPlatform/Core/Account/Controller/ExceptionController.php
@@ -8,11 +8,12 @@
 
 namespace NemesisPlatform\Core\Account\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\TwigBundle\Controller as TwigController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\FlattenException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 /**
@@ -20,43 +21,113 @@ use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
  *
  * @package NemesisPlatform\Game\Controller\Site
  */
-class ExceptionController extends TwigController\ExceptionController
+class ExceptionController extends Controller
 {
+    /**
+     * @var bool Show error (false) or exception (true) pages by default
+     */
+    protected $debug;
+
+    public function __construct(ContainerInterface $container, $debug)
+    {
+        $this->setContainer($container);
+        $this->debug = $debug;
+    }
+
     /**
      * Converts an Exception to a Response.
      *
      * @param Request              $request   The request
      * @param FlattenException     $exception A FlattenException instance
      * @param DebugLoggerInterface $logger    A DebugLoggerInterface instance
-     * @param string               $_format   The format to use for rendering (html, xml, ...)
      *
-     * @return Response
-     *
-     * @throws \InvalidArgumentException When the exception template does not exist
-     * @Template()
+     * @return Response|array
      */
-    public function showAction(
-        Request $request,
-        FlattenException $exception,
-        DebugLoggerInterface $logger = null,
-        $_format = 'html'
-    ) {
+    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null)
+    {
         $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
 
         $code = $exception->getStatusCode();
 
-        $http_exception = in_array(
-            'Symfony\Component\HttpKernel\Exception\HttpExceptionInterface',
-            class_implements($exception->getClass())
-        );
+        $http_exception = $exception instanceof HttpExceptionInterface;
 
-        return
+        return $this->render(
+            (string)$this->findTemplate($request, $request->getRequestFormat(), $code, $this->debug),
             [
                 'status_code'    => $code,
                 'status_text'    => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
                 'exception'      => $http_exception || $this->debug ? $exception : null,
                 'logger'         => $logger,
                 'currentContent' => $currentContent,
-            ];
+            ]
+        );
+    }
+
+
+    /**
+     * @param int $startObLevel
+     *
+     * @return string
+     */
+    protected function getAndCleanOutputBuffering($startObLevel)
+    {
+        if (ob_get_level() <= $startObLevel) {
+            return '';
+        }
+
+        Response::closeOutputBuffers($startObLevel + 1, true);
+
+        return ob_get_clean();
+    }
+
+    /**
+     * @param Request $request
+     * @param string  $format
+     * @param int     $code An HTTP response status code
+     * @param bool    $showException
+     *
+     * @return string
+     */
+    protected function findTemplate(Request $request, $format, $code, $showException)
+    {
+        $name = $showException ? 'exception' : 'error';
+        if ($showException && 'html' === $format) {
+            $name = 'exception_full';
+        }
+
+        // For error pages, try to find a template for the specific HTTP status code and format
+        if (!$showException) {
+            $template = sprintf('@NemesisCor/Exception/%s%s.%s.twig', $name, $code, $format);
+            if ($this->templateExists($template)) {
+                return $template;
+            }
+        }
+
+        // try to find a template for the given format
+        $template = sprintf('@NemesisCore/Exception/%s.%s.twig', $name, $format);
+        if ($this->templateExists($template)) {
+            return $template;
+        }
+
+        // default to a generic HTML exception
+        $request->setRequestFormat('html');
+
+        return sprintf('@NemesisCore/Exception/%s.html.twig', $showException ? 'exception_full' : $name);
+    }
+
+    // to be removed when the minimum required version of Twig is >= 3.0
+    protected function templateExists($template)
+    {
+        $template = (string)$template;
+
+        $loader = $this->get('twig.loader');
+        try {
+            $loader->getSource($template);
+
+            return true;
+        } catch (\Twig_Error_Loader $e) {
+        }
+
+        return false;
     }
 }
